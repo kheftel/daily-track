52ad0bd4754316e9d9ddb25f6c611e8819781733
Kawika Heftel <kawika@luckykatworld.com>
2019-03-29 11:23:31 -0600

    chore: get data from server

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 // requires
 const express = require('express'),
     app = express(),
-    path = require('path')
+    path = require('path'),
+    moment = require('moment')
 
 // const users = require('./users');
+
+// generate some data
+function generateDataSet(type = "line", label = "Meditation") {
+    var tempData = {
+        "type": type,
+        "label": label,
+        "data": generateTempData(),
+        "fill": false,
+        "borderColor": "rgb(75, 192, 192)",
+        "lineTension": 0
+    };
+    return tempData;
+}
+
+function generateTempData(numDays = 30, missPercentage = .3, maxValue = 30) {
+    var tempData = [];
+    for (var days = -numDays + 1; days <= 0; days++) {
+        tempData.push({
+            x: relativeDateString(days),
+            y: (Math.random() < missPercentage) ? 0 : Math.ceil(Math.random() * maxValue)
+        })
+    }
+    return tempData;
+}
+
+function relativeDateString(daysFromNow) {
+    return moment().add(daysFromNow, 'd').format('YYYY-MM-DD');
+}
+
+//var data = generateDataSet();
 
 //setting the port.
 app.set('port', process.env.PORT || 3000);
@@ -16,12 +47,12 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
 
-/*app.get('/users', (request, response) => {
-    response.json(users);
-});*/
+app.get('/api/data', (request, response) => {
+    response.json(generateDataSet());
+});
 
 var port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log('Express server started at port port');
+    console.log('Express server started at port ' + port);
 });
