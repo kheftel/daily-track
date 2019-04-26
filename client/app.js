b778f43d850d605d9567a70ef0abae6bf43d0296
Kawika Heftel <kawika@luckykatworld.com>
2019-04-26 10:54:17 -0600

    fix: regression, sidebar-shade blocked content

--- client/app.js
+++ client/app.js
@@ -38,6 +38,12 @@ window.helpers.createChartControllerFromId = function(container, id) {
 
 
 $(document).ready(function () {
+    // backdrop for sidebar to block touches on content
+    var sidebarbackdrop = document.createElement('div');
+    sidebarbackdrop.id = 'sidebar-shade';
+    $(sidebarbackdrop).addClass('modal-backdrop fade show d-md-none');
+
+    // click handler for menu button
     $('#sidebarCollapse').on('click', function () {
         // current state of sidebar
         var sidebarActive = $('#sidebar').hasClass('active');
@@ -47,13 +53,15 @@ $(document).ready(function () {
 
         if(sidebarActive) {
             $('#sidebar').addClass('active');
-            $('#sidebar-shade').addClass('show');
+            // $('#sidebar-shade').addClass('show');
+            document.body.appendChild(sidebarbackdrop);
             $('#content').addClass('blur blur-md-none');
             $('body').addClass('sidebar-active');
         }
         else {
             $('#sidebar').removeClass('active');
-            $('#sidebar-shade').removeClass('show');
+            // $('#sidebar-shade').removeClass('show');
+            document.body.removeChild(sidebarbackdrop);
             $('#content').removeClass('blur blur-md-none');
             $('body').removeClass('sidebar-active');
         }
